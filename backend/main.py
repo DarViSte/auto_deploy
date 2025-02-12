4820e2db264215a71f7245f66c4c469cd1cefcb3
Daria Stepanova <Daria.Stepanova@cancer.fi>
2025-02-12 13:10:14 +0200

    feat: backend next step

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
+from fastapi.middleware.cors import CORSMiddleware
+
+
+# Создаем экземпляр FastAPI
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
-    return {"message": "Hello, World!"}
+# Настроим CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Разрешаем запросы с любых источников (будьте осторожны на продакшн)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Модель для проверки данных
+class UserData(BaseModel):
+    name: str
+    email: str
+
+# Эндпоинт для приема данных
+@app.post("/submit")
+async def submit_data(user_data: UserData):
+    return {"name": user_data.name, "email": user_data.email}
